3aa409d0d99c26e38ad909d659f6b2433a818137
TimTernar <somthing900@gmail.com>
2026-03-18 13:40:36 +0100

    finished changing endpoints to work with mongoDB database instead of H2

--- src/main/java/com/primer/store/ProductController.java
+++ src/main/java/com/primer/store/ProductController.java
@@ -1,8 +1,6 @@
 package com.primer.store;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,36 @@ public class ProductController {
     @GetMapping
     public List<Product> getAllProducts() {
         return repository.findAll();
+    }
+
+    //najdi posebej
+    @GetMapping("/{id}")
+    public Product getOneProduct(@PathVariable String id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Product not found: " + id));
+    }
+
+    @PostMapping
+    public Product newProduct(@RequestBody Product newProduct) {
+        return repository.save(newProduct);
+    }
+
+    @PutMapping("/{id}")
+    public Product replaceProduct(@RequestBody Product newProduct, @PathVariable String id) {
+        return repository.findById(id)
+                .map(product -> {
+                    product.setName(newProduct.getName());
+                    product.setPrice(newProduct.getPrice());
+                    return repository.save(product);
+                })
+                .orElseGet(() -> {
+                    newProduct.setId(id);
+                    return repository.save(newProduct);
+                });
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDocument(@PathVariable String id)
+    {
+        repository.deleteById(id);
     }
 }
--- src/main/java/com/primer/store/ProductRepository.java
+++ src/main/java/com/primer/store/ProductRepository.java
@@ -3,7 +3,7 @@ package com.primer.store;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
-public interface ProductRepository extends MongoRepository<Product, Integer> {
+public interface ProductRepository extends MongoRepository<Product, String> {
     Optional<Product> findByName(String name);
 
     Optional<Product> findByPrice(double price);
