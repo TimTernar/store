2bf5fb37deb68fc4a6eed1a122da522a1090d387
TimTernar <somthing900@gmail.com>
2026-03-18 11:56:28 +0100

    removed old code

--- src/main/java/com/primer/store/LoadDatabase.java
+++ src/main/java/com/primer/store/LoadDatabase.java
@@ -11,18 +11,6 @@ public class LoadDatabase {
 
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-    //old JPA data i left behind
-    @Bean
-    CommandLineRunner initDatabase(ProductRepository repository)
-    {
-        return args -> {
-            log.info("Nalaganje produkta 4" + repository.save(new Product(null, "HP 4500", 1700)));
-            log.info("Nalaganje produkta 5" + repository.save(new Product(null, "Huawei Nekaj", 1200)));
-            log.info("Nalaganje produkta 6" + repository.save(new Product(null, "Bad Laptop", 8000)));
-        };
-    }
-
-
     public @Bean com.mongodb.client.MongoClient mongoClient() {
         return com.mongodb.client.MongoClients.create("mongodb://localhost:27017");
     }
